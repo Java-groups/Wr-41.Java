03b1c41e77936341ae770c8b378cf0c8399d9014
F1iper <ftimofiejew@gmail.com>
2022-10-11 15:16:02 +0200

    Initial project version

---
+++ src/test/java/com/softserve/SportsHub/SportsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.SportsHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
