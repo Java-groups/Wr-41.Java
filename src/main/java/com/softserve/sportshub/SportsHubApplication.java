67e10ee10d2442c95667605c818bdf0e86da3363
Pawel Miskowiec <pmiskowi@student.wszib.edu.pl>
2022-10-14 11:32:40 +0200

    forgot to add in last commit

--- src/main/java/com/softserve/sportshub/SportsHubApplication.java
+++ src/main/java/com/softserve/sportshub/SportsHubApplication.java
@@ -1,25 +1,14 @@
-package com.softserve.SportsHub;
+package com.softserve.sportshub;
 
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class SportsHubApplication {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SportsHubApplication.class);
-
-		Test testBean = ctx.getBean(Test.class);
-		System.out.println(testBean.getMessage());
-
 	}
 
-	@Bean
-	public String giveMeString(){
-		return "Hello";
-	}
 }
