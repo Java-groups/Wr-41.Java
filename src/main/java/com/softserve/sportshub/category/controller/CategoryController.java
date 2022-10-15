77486112e2e4a39c26f4bbcf4dd9efcee8ab146b
Remigiusz Królak <krolak.remek@gmail>
2022-10-15 21:10:57 +0200

    Added @RequestBody annotation in PostMapping controller.

--- src/main/java/com/softserve/sportshub/category/controller/CategoryController.java
+++ src/main/java/com/softserve/sportshub/category/controller/CategoryController.java
@@ -7,10 +7,7 @@ import com.softserve.sportshub.category.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,7 +26,7 @@ public class CategoryController {
     }
 
     @PostMapping
-    public ResponseEntity<CategoryDto> add(CreateCategoryCommand command) {
+    public ResponseEntity<CategoryDto> add(@RequestBody CreateCategoryCommand command) {
         categoryService.save(new Category(command.getName()));
         return ResponseEntity.status(HttpStatus.CREATED).body(new CategoryDto(command.getName()));
     }
